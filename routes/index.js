15d4afb8cf1059392bbd7810d00d91de07a89ec6
Alicja Slupska <ala.slupska@gmail.com>
2021-07-21 01:26:56 +0200

    plants images added to the public folder, json data structure modified, GET methods modfied in order to get correct image URI

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,16 @@ const plants_demo_data = require('../plants-demo-data.json');
 /* GET the page with all plants. */
 router.get('/api/v1/plants/search', (req, res, next) => {
 
+  const apiToken = req.query.token;
+
+  if (apiToken !== process.env.DEMO_API_ACCESS_TOKEN) {
+    res.status(401).json({ error: "Invalid API token!" });
+    return;
+  }
+
   let term = req.query.q;
   if (typeof term === 'string') {
-    term = term.trim();
+    term = term.trim().toLowerCase();
     if (term.length === 0) {
       term = undefined;
     }
@@ -21,15 +28,16 @@ router.get('/api/v1/plants/search', (req, res, next) => {
 
   const data = plants_demo_data.data.filter(plant =>
     !term ||
-    plant.common_name.includes(term) ||
-    plant.scientific_name.includes(term) ||
-    plant.synonyms.reduce((found, synonym) => found || synonym.includes(term), false)
+    plant.common_name.toLowerCase().includes(term) ||
+    plant.scientific_name.toLowerCase().includes(term) ||
+    plant.synonyms.reduce((found, synonym) => found || synonym.toLowerCase().includes(term), false)
   );
 
   const pageSize = 20;
   const begin = (pageNum - 1) * pageSize; // pageNum * pageSize - pageSize
   const end = begin + pageSize - 1;
-  const pageData = data.slice(begin, end);
+  const pageData = data.slice(begin, end)
+    .map(plant => ({ ...plant, image_url: process.env.HOST_URI + plant.image_url }));
 
   res.json({ data: pageData, meta: { total: data.length } });
 });
@@ -37,6 +45,14 @@ router.get('/api/v1/plants/search', (req, res, next) => {
 
 /* GET single plant page. */
 router.get('/api/v1/plants/:slug', (req, res, next) => {
+
+  const apiToken = req.query.token;
+
+  if (apiToken !== process.env.DEMO_API_ACCESS_TOKEN) {
+    res.status(401).json({ error: "Invalid API token!" });
+    return;
+  }
+
 
   let apiSlug = decodeURIComponent(req.params.slug);
 
@@ -47,12 +63,31 @@ router.get('/api/v1/plants/:slug', (req, res, next) => {
 
   const plantData = plants_demo_data.data.find(plant => plant.slug === apiSlug);
 
-  if(!plantData) {
+  if (!plantData) {
     res.status(404).json({ error: "Unknown slug!" });
     return;
   }
 
-  res.json({ data: plantData })
+  const flowerImages = plantData.main_species.images.flower
+    .map(imgObj => ({ // using () around {} gives back an object
+      ...imgObj,
+      image_url: process.env.HOST_URI + imgObj.image_url,
+    }));
+
+  // override recursive modified values
+  res.json({
+    data: {
+      ...plantData,
+      image_url: process.env.HOST_URI + plantData.image_url,
+      main_species: {
+        ...plantData.main_species,
+        images: {
+          ...plantData.main_species.images,
+          flower: flowerImages
+        }
+      }
+    }
+  })
 
 });
 
